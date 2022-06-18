8b459e4c35038c3e7250c26cb85c256ea053aa75
tsukiroku <tsukiroku@yahoo.com>
2022-06-18 21:29:53 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,50 +1,40 @@
 import express from 'express';
-import ip from 'ip';
 import fs from 'fs';
 import colors from 'colors';
-import { Request } from 'express-serve-static-core';
-import { ParsedQs } from 'qs';
+import log from './src/logger';
 
 const app = express();
 
-export default (file: string, port: number) => {
+export default (file: string | null, port: number) => {
     colors.enable();
-    if (!fs.existsSync(file)) {
-        console.log(`File '${file}' not found.`.red);
-        return;
-    }
+    if (!file) console.log(colors.red('No file specified'));
+    else if (!fs.existsSync(file)) console.log(`File '${file}' not found.`.red);
+    else if (fs.lstatSync(file).isDirectory())
+        console.log(`'${file}' is a directory.`.red);
+    else {
+        app.get(`/`, (req, res) => {
+            res.attachment(file);
+            fs.createReadStream(file).pipe(res);
+            log(req);
+        });
 
-    const log = (
-        req: Request<{}, any, any, ParsedQs, Record<string, any>>,
-        suffix?: string
-    ) =>
+        app.get(`/raw`, (req, res) => {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            fs.createReadStream(file).pipe(res);
+            log(req, 'raw');
+        });
+
+        app.listen(port);
+
+        const url = `http://${import('ip').then((m) => m.address())}:${
+            `${port}`.gray
+        }`.underline;
+        const share = 'share'.bgBlack.white;
+        const to = '➜'.red;
         console.log(
-            `${
-                `[${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}]`
-                    .green
-            } ${req.ip} ${suffix ? `(${suffix.blue})` : ''}`
+            `${share} File uploaded to ${to} ${url}\n${share} Raw data ${to} ${url}${
+                '/raw'.underline.bold
+            }\n`
         );
-
-    app.get(`/`, (req, res) => {
-        res.attachment(file);
-        fs.createReadStream(file).pipe(res);
-        log(req);
-    });
-
-    app.get(`/raw`, (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        fs.createReadStream(file).pipe(res);
-        log(req, 'raw');
-    });
-
-    app.listen(port);
-
-    const url = `http://${ip.address()}:${`${port}`.gray}`.underline;
-    const share = 'share'.bgBlack.white;
-    const to = '➜'.red;
-    console.log(
-        `${share} File uploaded to ${to} ${url}\n${share} Raw data ${to} ${url}${
-            '/raw'.underline.bold
-        }\n`
-    );
+    }
 };
